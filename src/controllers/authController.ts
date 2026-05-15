d196a5ed9d8ef091fdaa7b5da46e93fb6611c5ef
Julio <bjulio52@gmail.com>
2026-05-15 16:24:43 -0300

    feat: inicia integracao com front

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
-import prisma from "../../prisma/client"
+import prisma from "../lib/prisma/client"
 
 export async function cadastrar(req: Request, res: Response) {
   try {
